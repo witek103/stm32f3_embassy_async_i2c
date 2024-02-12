85904c500be9b8985ab6edbe14d34fad638ff904
Witold Lipieta <witek103@gmail.com>
2024-02-12 23:04:27 +0100

    Use mpu-6500-async crate to read gyroscope measurements from IMU
    Add mpu-6500-async to dependencies.
    Implement IMU initialization and enable data ready interrupt.
    Use async for non blocking gyroscope data readout triggered by the
    interrupt.
    
    Signed-off-by: Witold Lipieta <witek103@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,62 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_time::Timer;
+use embassy_stm32::exti::ExtiInput;
+use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
+use embassy_stm32::i2c::I2c;
+use embassy_stm32::time::Hertz;
+use embassy_stm32::{bind_interrupts, i2c, peripherals};
+use mpu_6500_async::{Mpu6500};
 use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    I2C1_EV => i2c::EventInterruptHandler<peripherals::I2C1>;
+    I2C1_ER => i2c::ErrorInterruptHandler<peripherals::I2C1>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
     let mut led = Output::new(p.PC13, Level::High, Speed::Low);
+    let mut imu_interrupt = ExtiInput::new(Input::new(p.PB4, Pull::Up), p.EXTI4);
+
+    let i2c = I2c::new(
+        p.I2C1,
+        p.PA15,
+        p.PB7,
+        Irqs,
+        p.DMA1_CH6,
+        p.DMA1_CH7,
+        Hertz(400_000),
+        Default::default(),
+    );
+
+    let mut imu = Mpu6500::new(i2c);
+
+    if !imu.detected().await.unwrap() {
+        defmt::panic!("IMU not detected!");
+    }
+
+    info!("IMU detected");
+
+    imu.init(&mut embassy_time::Delay).await.unwrap();
+
+    info!("IMU initialized");
+
+    imu.enable_data_ready_interrupt().await.unwrap();
+
+    info!("IMU interrupts enabled");
 
     loop {
-        info!("Turn on LED");
+        imu_interrupt.wait_for_low().await;
 
         led.set_high();
 
-        Timer::after_millis(1000).await;
-
-        info!("Turn off LED");
+        let gyro = imu.read_gyro_data().await.unwrap();
 
         led.set_low();
 
-        Timer::after_millis(1000).await;
+        info!("Gyro raw data: {} {} {}", gyro.x(), gyro.y(), gyro.z());
     }
 }
