4c725127832bd08f9838543aa665847e619ce439
Witold Lipieta <witek103@gmail.com>
2024-02-20 23:52:22 +0100

    Use vl53l1x_async crate to implement distance sensor readout
    Add vl53l1x_async crate to dependencies.
    Add new task for distance sensor initialization and data readout.
    Sensor is configured to work in non-blocking interrupt mode with
    ~15ms measurement interval.
    
    Both VL53L1X and MPU6500 are working concurrently on the same
    I2C bus.
    
    Signed-off-by: Witold Lipieta <witek103@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ use embassy_sync::mutex::Mutex;
 use embassy_time::Timer;
 use mpu_6500_async::{calibration, raw_data::RawData, Mpu6500};
 use static_cell::StaticCell;
+use vl53l1x_async::register_map::{DistanceMode, TimingBudget};
+use vl53l1x_async::Vl53l1x;
 use {defmt_rtt as _, panic_probe as _};
 
 type I2c1<'a> = I2c<'a, peripherals::I2C1, peripherals::DMA1_CH6, peripherals::DMA1_CH7>;
@@ -30,7 +32,18 @@ async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
     let mut led = Output::new(p.PC13, Level::High, Speed::Low);
+    let mut fl_en = Output::new(p.PB8, Level::Low, Speed::Low);
+    let mut fr_en = Output::new(p.PA1, Level::Low, Speed::Low);
+    let mut dl_en = Output::new(p.PB9, Level::Low, Speed::Low);
+    let mut dr_en = Output::new(p.PA0, Level::Low, Speed::Low);
+
     let imu_interrupt = ExtiInput::new(Input::new(p.PB3, Pull::Up), p.EXTI3);
+    let ds_interrupt = ExtiInput::new(Input::new(p.PA4, Pull::Down), p.EXTI4);
+
+    fl_en.set_low();
+    fr_en.set_low();
+    dl_en.set_low();
+    dr_en.set_low();
 
     let i2c = I2c::new(
         p.I2C1,
@@ -47,7 +60,11 @@ async fn main(spawner: Spawner) {
 
     let imu = Mpu6500::new(I2cDevice::new(i2c_bus));
 
+    let ds = Vl53l1x::new(I2cDevice::new(i2c_bus)).with_x_shut(fl_en);
+
     unwrap!(spawner.spawn(imu_task(imu, imu_interrupt)));
+
+    unwrap!(spawner.spawn(distance_sensor_task(ds, ds_interrupt)));
 
     loop {
         led.set_high();
@@ -103,5 +120,53 @@ async fn imu_task(
         let gyro = imu.read_gyro_data().await.unwrap();
 
         info!("Gyro raw data: {} {} {}", gyro.x(), gyro.y(), gyro.z());
+    }
+}
+
+#[embassy_executor::task]
+async fn distance_sensor_task(
+    mut ds: Vl53l1x<
+        I2cDevice<'static, NoopRawMutex, I2c1<'static>>,
+        Output<'static, peripherals::PB8>,
+    >,
+    mut ds_interrupt: ExtiInput<'static, peripherals::PA4>,
+) {
+    info!("Sensor task started");
+
+    ds.reset_until_detection(&mut embassy_time::Delay, 1000)
+        .await
+        .unwrap();
+
+    info!("Sensor turned on");
+
+    ds.wait_until_booted(&mut embassy_time::Delay, 1000)
+        .await
+        .unwrap();
+
+    info!("Sensor booted");
+
+    ds.init(&mut embassy_time::Delay, 1000).await.unwrap();
+
+    info!("Sensor initialized");
+
+    ds.set_interrupt_polarity(true).await.unwrap();
+    ds.set_distance_mode(DistanceMode::Short).await.unwrap();
+    ds.set_timing_budget(TimingBudget::Ms15).await.unwrap();
+    ds.set_inter_measurement_period(15).await.unwrap();
+
+    info!("Sensor configured");
+
+    ds.start_ranging().await.unwrap();
+
+    info!("Ranging started");
+
+    loop {
+        ds_interrupt.wait_for_high().await;
+
+        let distance = ds.get_distance().await.unwrap();
+
+        ds.clear_interrupt().await.unwrap();
+
+        info!("Distance sensor data: {}", distance);
     }
 }
