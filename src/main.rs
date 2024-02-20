d7586936c9c304e72e7e0536b2f53ad20d368333
Witold Lipieta <witek103@gmail.com>
2024-02-20 23:21:18 +0100

    Move IMU initialization and data readout to separate task
    Add embassy_embedded_hal and static_cell to dependencies. Use features
    provided by the crates to share access to I2C bus between tasks - it
    will allow to communicate with many devices on the bus.
    
    Move IMU related code to separate task and spawn in in main thread.
    
    Main task is now just blinking a led, gyro readouts are done in
    imu_task.
    
    Signed-off-by: Witold Lipieta <witek103@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 #![no_std]
 #![no_main]
 
-use defmt::info;
+use defmt::{info, unwrap};
+use embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice;
 use embassy_executor::Spawner;
 use embassy_stm32::exti::ExtiInput;
 use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
 use embassy_stm32::i2c::I2c;
 use embassy_stm32::time::Hertz;
 use embassy_stm32::{bind_interrupts, i2c, peripherals};
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use embassy_sync::mutex::Mutex;
+use embassy_time::Timer;
 use mpu_6500_async::{calibration, raw_data::RawData, Mpu6500};
+use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
+
+type I2c1<'a> = I2c<'a, peripherals::I2C1, peripherals::DMA1_CH6, peripherals::DMA1_CH7>;
+
+static I2C1_BUS: StaticCell<Mutex<NoopRawMutex, I2c1>> = StaticCell::new();
 
 bind_interrupts!(struct Irqs {
     I2C1_EV => i2c::EventInterruptHandler<peripherals::I2C1>;
@@ -17,11 +26,11 @@ bind_interrupts!(struct Irqs {
 });
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
     let mut led = Output::new(p.PC13, Level::High, Speed::Low);
-    let mut imu_interrupt = ExtiInput::new(Input::new(p.PB3, Pull::Up), p.EXTI3);
+    let imu_interrupt = ExtiInput::new(Input::new(p.PB3, Pull::Up), p.EXTI3);
 
     let i2c = I2c::new(
         p.I2C1,
@@ -34,7 +43,29 @@ async fn main(_spawner: Spawner) {
         Default::default(),
     );
 
-    let mut imu = Mpu6500::new(i2c);
+    let i2c_bus = I2C1_BUS.init(Mutex::new(i2c));
+
+    let imu = Mpu6500::new(I2cDevice::new(i2c_bus));
+
+    unwrap!(spawner.spawn(imu_task(imu, imu_interrupt)));
+
+    loop {
+        led.set_high();
+
+        Timer::after_millis(1000).await;
+
+        led.set_low();
+
+        Timer::after_millis(1000).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn imu_task(
+    mut imu: Mpu6500<I2cDevice<'static, NoopRawMutex, I2c1<'static>>>,
+    mut imu_interrupt: ExtiInput<'static, peripherals::PB3>,
+) {
+    info!("IMU task started");
 
     if !imu.detected().await.unwrap() {
         defmt::panic!("IMU not detected!");
@@ -69,11 +100,7 @@ async fn main(_spawner: Spawner) {
     loop {
         imu_interrupt.wait_for_low().await;
 
-        led.set_high();
-
         let gyro = imu.read_gyro_data().await.unwrap();
-
-        led.set_low();
 
         info!("Gyro raw data: {} {} {}", gyro.x(), gyro.y(), gyro.z());
     }
